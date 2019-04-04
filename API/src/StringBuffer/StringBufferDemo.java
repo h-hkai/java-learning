5c3b402aa4705075f466a9770281ef48add8ec8b
studycat <1376253264@qq.com>
2019-04-04 20:18:56 +0800

    StringBuffer

--- API/src/StringBuffer/StringBufferDemo.java
+++ API/src/StringBuffer/StringBufferDemo.java
@@ -3,13 +3,34 @@ package StringBuffer;
 public class StringBufferDemo {
 
 	public static void main(String[] args) {
-		
+		function();
 	}
 	
 	public static void function() {
 		StringBuffer buffer = new StringBuffer();
 		
-		buffer.append(6);
+		buffer.append(6).append(false).append("Tom");
+		System.out.println(buffer);
+		
+		buffer.delete(3, 4);
+		System.out.println(buffer);
+		
+		buffer.delete(0, buffer.length());
+		System.out.println(buffer);
+		
+		buffer.append("append");
+		buffer.insert(3, 9);
+		System.out.println(buffer);
+		
+		buffer.replace(1, 4, "Q");
+		System.out.println(buffer);
+		
+		buffer.reverse();
+		System.out.println(buffer);
+		
+		buffer.append(1357);
+		String s = buffer.toString();
+		System.out.println(s);
 	}
 
 }
